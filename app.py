99cb64389ac8004eb4698205d44a6b88a40bd38a
ri-gh <98031978+ri-gh@users.noreply.github.com>
2023-02-03 16:17:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,34 +20,40 @@ list_month = sorted(list_month, key=lambda t: datetime.datetime.strptime(t,'%B')
 list_month = ['Whole year'] + list_month #to add the "whole year" option
 list_hour = df['hour'].unique().tolist()
 
-#Show a default graph when opening the dash app we create a default df for Hollywood in May 2020 between 2 & 8 am:
-df_default = df[df['year'] == 2020].reset_index(drop=True)
-df_default = df_default[df_default['month_name'] == 'May'].reset_index(drop=True)
-df_default = df_default[df_default['AREA NAME'] == 'Hollywood'].reset_index(drop=True)
-start_hour = 2 #to have the first hour selected from the range slider
+#Show a default graph when opening the dash app we create a default df for Hollywood in May 2020 between 5 & 8 am:
+month_choose = 'May'
+year_choose = 2020
+area = 'Hollywood'
+df_default = df[df['year'] == year_choose].reset_index(drop=True)
+df_default = df_default[df_default['month_name'] == month_choose].reset_index(drop=True)
+df_default = df_default[df_default['AREA NAME'] == area].reset_index(drop=True)
+start_hour = 5 #to have the first hour selected from the range slider
 end_hour = 8   #to have the last hour selected from the range slider
 df_default = df_default[(df_default['hour'] >=  start_hour) & (df_default['hour'] <= end_hour)].reset_index(drop=True)
 df_default = df_default.reset_index(drop=True)
 
-def default_pie():
+def default_pie(): 
     df_pie_default = df_default.groupby('Type of crime')['Crm Cd Desc'].count()
     df_pie_default = df_pie_default.reset_index()
     df_pie_default = pd.DataFrame(df_pie_default)
 
     fig_pie_default = px.pie(df_pie_default,values='Crm Cd Desc',names='Type of crime',
-    title='Repartition of crime type for Hollywood',)
+    title='Crime type repartition for {} area'.format(area))
     fig_pie_default.update_traces(textposition='inside', textinfo='percent+label',#to put the label inside the pie
-    hovertemplate="<br>".join(["Period: {} {}".format(df_default['month_name'][0],df_default['year'][0]),
+    hovertemplate="<br>".join(["Period: {} {}".format(month_choose,year_choose),
         "Hours range: between {}h and {}h".format(start_hour,end_hour),
         "Type of crime: %{label}",
         "Number: %{value}"] ))
-    fig_pie_default.update_traces(hole=.4) #change from pie to donut chart
+    fig_pie_default.update_traces(hole=.4) #to change the pie chart to donut chart
     fig_pie_default.update_layout(legend_title_text= "Type of crime:",
     template='plotly_dark',
-    legend=dict(font=dict(color="white")),
+    legend=dict(font=dict(color="white")), #to change color of the legend 
     title_font_color="rgb(159, 241, 253)", #to change the color of the figure title
     paper_bgcolor='black', #to change the background color of the figure
     plot_bgcolor='black')#to change the background colour of the graph
+    fig_pie_default.add_annotation(text= 'Between {}h & {}h in {} {}'.format(start_hour,end_hour,month_choose,year_choose),
+    showarrow=False, y = -0.2,
+    font=dict(color="rgb(159, 241, 253)",size = 15)) #to add text under the donut chart
 
     return fig_pie_default
 
@@ -64,14 +70,16 @@ def default_bar():
         "Period : {} {} ".format(df_line_day_name['month_name'][0],df_line_day_name['year'][0]),
         "Range of hour: between {}h and {}h".format(start_hour,end_hour),
         "Number of crime: %{y}"] ))
+    fig_day_name_area_default.update_xaxes(color="rgb(159, 241, 253)",showgrid = False) #change color of xaxes labels
+    fig_day_name_area_default.update_yaxes(color="white") #change color of yaxes labels
     fig_day_name_area_default.update_layout(legend_title_text= "Area:",
-    xaxis_title="May 2020 between 2h and 8h",
+    xaxis_title="May 2020 between 5h and 8h",
     yaxis_title="Number of crime",
     template='plotly_dark',
+    legend=dict(font=dict(color="white")), 
     title_font_color="rgb(159, 241, 253)",
     paper_bgcolor='black', #to change the background color of the figure
     plot_bgcolor='black')#to change the background colour of the graph
-    fig_day_name_area_default.update_xaxes(color="rgb(159, 241, 253)",showgrid = False) #change color of xaxes labels
 
     return fig_day_name_area_default
 
@@ -81,7 +89,7 @@ def default_fig_line():
     df_plok= df_plok.reset_index() # to have the multi-index data as columns of this df
 
     fig_line_default = px.line(df_plok, x="hour", y='Crm Cd Desc', color='AREA NAME',
-    title= "Crime number per hour between 2h and 8h in May 2020",
+    title= "Crime number per hour between 5h and 8h in May 2020",
     custom_data=['AREA NAME','hour','Crm Cd Desc'])
     fig_line_default.update_layout(legend_title_text= "Area:",
     xaxis_title="Hours",
@@ -99,15 +107,16 @@ def default_fig_line():
         ])
     )
     fig_line_default.update_xaxes(color="rgb(159, 241, 253)") #change color of xaxes labels
+    fig_line_default.update_yaxes(color="white") #change color of yaxes labels
 
     return fig_line_default
 
 def default_fig():
     fig_default = px.scatter_mapbox(df_default, lat="LAT", lon="LON",color='AREA NAME',size= 'Vict Age',mapbox_style="carto-darkmatter",
-    title="LA's areas crime map for May 2020 between 2h and 8h",
+    title="LA's areas crime map for May 2020 between 5h and 8h",
     custom_data=['AREA NAME','Vict Age','Vict Sex','Crm Cd Desc','TIME OCC','DATE OCC','LOCATION'],
     center={'lat': 34.052235, 'lon': -118.243683},#to center the map on LA
-    zoom=7)
+    zoom=6)
     fig_default.update_traces(
     hovertemplate="<br>".join([
         "Area Name: %{customdata[0]}",
@@ -141,30 +150,31 @@ app.layout = html.Div([
             dbc.CardBody([
                 html.Div([html.Img(src=logo, height="150px"),
                 html.P(),
-
+                    
                             dbc.Row(
                 [ html.H2("Los Angeles areas crime report analysis from 2020 until today",style={'textAlign': 'center'}), #to add an header text line  #logo
                     dbc.Col(),
 
                 ])], style={'textAlign': 'center',
-                'color':'rgb(159, 241, 253)','background-color' : 'black'},                #to change color of the written font
+                'color':'rgb(159, 241, 253)', 'background-color':'black'},                #to change color of the written font
 
                 )
+                
 
-
-            ]), color = 'dark'
-        )
-    ]),
+            ],style={'background-color':'dark'})
+        ),
+    ],style={'background-color':'dark'}),
     ]),
     dbc.Row([
-    dbc.Col([dbc.Button(html.Div(children=[
+    dbc.Col([dbc.Button(html.Div(children=[ 
     html.Div(children=html.H5(children="Select year:"),style={'color':'rgb(159, 241, 253)'}),
-    dcc.Dropdown(id="select-year",
+    dcc.Dropdown(
     options=sorted([{"label": year, "value":year} for year in list_year], key = lambda x: x['label']),
     value='',
     multi = False,
     style={'color': 'Blue','background-color':'rgb(159, 241, 253)'},
-    placeholder='Select a year...'),
+    placeholder='Select a year...',
+    id="select-year"),
     html.Div(id="month-select-div",style={'color':'rgb(159, 241, 253)'}),
     dcc.Dropdown(id="select-month",
     options = [month for month in list_month], #we didn't sorted the month alphabetically because we already sorted them by them ranking upper
@@ -175,7 +185,7 @@ app.layout = html.Div([
 
     html.Br(),
     html.Div(id="select-hour-div",style={'color':'rgb(159, 241, 253)'}),
-    dcc.RangeSlider(min(list_hour),max(list_hour),1,value=[2,8],allowCross=False,
+    dcc.RangeSlider(min(list_hour),max(list_hour),1,allowCross=False,
     tooltip={"placement": "top", "always_visible": False},
     marks={0:{'label':'00', 'style': {'color': '#77b0b1'}},
     1:{'label':'01'},
@@ -215,15 +225,15 @@ app.layout = html.Div([
     style={'color': 'Blue','background-color':'rgb(159, 241, 253)'},
     placeholder='Select area...',
     id="select-area"),
-
+  
     dcc.Checklist(id='select-all',                        #to add a select all tick box
     options=[{'label': 'Select all areas', 'value': 1}]),
     ],style={'background-color':'black'},
     ),
     style={"width":"100%","height": "100%",'color':'rgb(159, 241, 253)'})],width=4, #layout width are between 0 & 12
-    align='center'),
+    align='center'), 
 
-
+    
     dbc.Col([dbc.Button(dcc.Graph(id="graph_area_fig_pie",figure=default_pie()),style={"width":"100%","height": "100%"})],width=4,
     align='center'),
     #to set up height of the component
@@ -233,16 +243,17 @@ app.layout = html.Div([
     dbc.Row([
     dbc.Col([dbc.Button(dcc.Graph(id="graph_per_hour_line",figure=default_fig_line()), style={"width": "100%","height": "100%"})],width=6,
     align='center'),
-    dbc.Col([dbc.Button(dcc.Graph(id="graph_per_hour_map",figure= default_fig()),style={"width": "100%","height": "100%"})],width=6)
+    dbc.Col([dbc.Button(dcc.Graph(id="graph_per_hour_map",figure=default_fig()),style={"width": "100%","height": "100%"})],width=6)
     ], align='center'),
 ]), color = 'dark'
     ),
         html.Br(),
     dbc.Row([
-        dbc.Col([dbc.Button(html.Img(src=logo_dash, height="50px"),style={"width": "100%","height": "100%", "textAlign" :"left"})])
+        dbc.Col([dbc.Button(html.Img(src=logo_dash, height="50px"),style={'color':'black',"width": "100%","height": "100%", "textAlign" :"left",
+        'background-color':'black'})
+            ],style={'color':'black','background-color':'black'})
 
-    ])
-    ],style={'background-color':'dark'})
+    ],style={'color':'black','background-color':'black'}) ],style={'color':'black','background-color':'black'})
 
 
 @app.callback(dash.dependencies.Output("month-select-div", "children"),
@@ -262,13 +273,14 @@ def display_month_year(month, year):
         return html.H5(children="Select a range of hours in {} {}:".format(month,year))
 
 
+
 @app.callback(dash.dependencies.Output("my_range_slider", "children"),
         dash.dependencies.Input("my_range_slider", "value"))
 def display_hour_range(hour_choice):
-    # We return an H5 HTML component with the range of hours selected.
+    # We return an H5 HTML component with the range of hour selected upper.
     return html.P(),html.Br(),html.H5(children="Select area between {}h and {}h :".format(hour_choice[0],hour_choice[1]))
 
-#to add a 'select all' tick box
+#to add a select all tick box
 @app.callback(
     dash.dependencies.Output('select-area', 'value'),
     [dash.dependencies.Input('select-all', 'value')],
@@ -291,20 +303,20 @@ def test(selected, options):
     dash.dependencies.Input('my_range_slider', 'value'))
 
 def pie(month_choose, year_choose,area,hour_choice):
-        if month_choose == 'Whole year':
-            df_area = df[df['month_name'] != month_choose].reset_index(drop=True)
-            df_area = df_area[df_area['year'] == year_choose].reset_index(drop=True)
-            df_area = df_area[df_area['AREA NAME'].isin(area)].reset_index(drop=True)
-            start_hour = hour_choice[0] #to have the first hour selected from the range slider
-            end_hour = hour_choice[1]   #to have the last hour selected from the range slider
-            df_area = df_area[(df_area['hour'] >=  start_hour) & (df_area['hour'] <= end_hour)].reset_index(drop=True)
 
+        df_area = df[df['year'] == year_choose].reset_index(drop=True)
+        df_area = df_area[df_area['AREA NAME'].isin(area)].reset_index(drop=True)          
+        start_hour = hour_choice[0] #to have the first hour selected from the range slider
+        end_hour = hour_choice[1]   #to have the last hour selected from the range slider
+        df_area = df_area[(df_area['hour'] >=  start_hour) & (df_area['hour'] <= end_hour)].reset_index(drop=True)
+
+        if month_choose == 'Whole year':
 
             df_pie_area = df_area.groupby('Type of crime')['Crm Cd Desc'].count()
             df_pie_area = pd.DataFrame(df_pie_area)
             df_pie_area = df_pie_area.reset_index()
             fig_pie = px.pie(df_pie_area,values='Crm Cd Desc',names='Type of crime',
-            title='Repartition of crime type for all selected area in {}'.format(year_choose))
+            title='Crime type repartition for selected area in {}'.format(year_choose))
             fig_pie.update_traces(textposition='inside', textinfo='percent+label',#to put the label inside the pie
             hole=.4,
             hovertemplate="<br>".join(["Year: {}".format(year_choose),
@@ -318,6 +330,9 @@ def pie(month_choose, year_choose,area,hour_choice):
             plot_bgcolor='black')#to change the background colour of the graph
             fig_pie.update_xaxes(showgrid = False, showticklabels = False, zeroline=False)
             fig_pie.update_yaxes(showgrid = False, showticklabels = False, zeroline=False)
+            fig_pie.add_annotation(text= 'Between {}h & {}h in {}'.format(start_hour,end_hour,year_choose),
+            showarrow=False, y = -0.2,
+            font=dict(color="rgb(159, 241, 253)",size = 15))
 
             df_line_day_name = df_area.groupby(['AREA NAME','year','day_name','day_ranking'])['Crm Cd Desc'].count().reset_index()
             df_line_day_name = pd.DataFrame(df_line_day_name)
@@ -331,7 +346,7 @@ def pie(month_choose, year_choose,area,hour_choice):
                 'Range of hours: between {}h and {}h'.format(start_hour,end_hour),
                 "Number of crime: %{y}"] ))
             fig_day_name_area.update_layout(legend_title_text= "Area:",
-            xaxis_title="In {} between {}h and {}h".format(year_choose,start_hour,end_hour),
+            xaxis_title="Between {}h and {}h in {}".format(start_hour,end_hour,year_choose),
             yaxis_title="Number of crime",
             template='plotly_dark',
             title_font_color="rgb(159, 241, 253)",
@@ -392,19 +407,13 @@ def pie(month_choose, year_choose,area,hour_choice):
             return fig_pie, fig_day_name_area, fig_line, fig
 
         else:
-            df_area = df[df['month_name'] == month_choose].reset_index(drop=True)
-            df_area = df_area[df_area['year'] == year_choose].reset_index(drop=True)
-            df_area = df_area[df_area['AREA NAME'].isin(area)].reset_index(drop=True)
-            start_hour = hour_choice[0] #to have the first hour selected from the range slider
-            end_hour = hour_choice[1]   #to have the last hour selected from the range slider
-            df_area = df_area[(df_area['hour'] >=  start_hour) & (df_area['hour'] <= end_hour)].reset_index(drop=True)
-
+            df_area = df_area[df_area['month_name'] == month_choose].reset_index(drop=True)
 
             df_pie_area = df_area.groupby('Type of crime')['Crm Cd Desc'].count()
             df_pie_area = pd.DataFrame(df_pie_area)
             df_pie_area = df_pie_area.reset_index()
             fig_pie = px.pie(df_pie_area,values='Crm Cd Desc',names='Type of crime',
-            title='Repartition of crime type for all selected area')
+            title='Crime type repartition for selected area')
             fig_pie.update_traces(textposition='inside', textinfo='percent+label',#to put the label inside the pie
             hole=.4,
             hovertemplate="<br>".join(["Period: {} {}".format(month_choose,year_choose),
@@ -418,6 +427,9 @@ def pie(month_choose, year_choose,area,hour_choice):
             plot_bgcolor='black')#to change the background colour of the graph
             fig_pie.update_xaxes(showgrid = False, showticklabels = False, zeroline=False)
             fig_pie.update_yaxes(showgrid = False, showticklabels = False, zeroline=False)
+            fig_pie.add_annotation(text= 'Between {}h & {}h in {} {}'.format(start_hour,end_hour,month_choose,year_choose),
+            showarrow=False, y = -0.2,
+            font=dict(color="rgb(159, 241, 253)",size = 15))
 
             df_line_day_name = df_area.groupby(['AREA NAME','month_name','year','day_name','day_ranking'])['Crm Cd Desc'].count().reset_index()
             df_line_day_name = pd.DataFrame(df_line_day_name)
@@ -492,6 +504,5 @@ def pie(month_choose, year_choose,area,hour_choice):
             return fig_pie, fig_day_name_area, fig_line, fig
 
 
-
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
